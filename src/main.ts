8aa9b3f431930d14f1a9b4ad4edc669d6ad5f398
Joachim Jasmin <joachim.jasmin@gmail.com>
2024-03-05 16:18:37 +0100

    feat: added cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    const allowedOrigins = [process.env.FRONTEND_URL_LOCAL];
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true,
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(corsOptions);
+
   await app.listen(4000);
 }
 bootstrap();
