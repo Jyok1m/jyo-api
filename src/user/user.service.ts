a1de8c5541cd419c83dbd73ec7633cb03d631753
Joachim Jasmin <joachim.jasmin@gmail.com>
2024-03-06 11:07:35 +0100

    feat: import du modèle User

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { Model } from "mongoose";
+import { InjectModel } from "@nestjs/mongoose";
+import { User } from "src/schemas/user.schema";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  // Import du modèle User
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+}
