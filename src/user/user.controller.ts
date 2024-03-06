dfb566bd2aeafae124875951b2ec7917b3eeb29d
Joachim Jasmin <joachim.jasmin@gmail.com>
2024-03-06 12:33:58 +0100

    feat: init controller for user creation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post, Body } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UserService } from "./user.service";
+import { User } from "src/schemas/user.schema";
 
 @Controller("user")
-export class UserController {}
+export class UserController {
+  // Import user service
+  constructor(private readonly userService: UserService) {}
+
+  @Post() // Post handler
+  async createUser(@Body() req: CreateUserDto): Promise<User> {
+    return this.userService.createUser(req);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, ConflictException } from "@nestjs/common";
 import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { User } from "src/schemas/user.schema";
@@ -6,12 +6,17 @@ import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
 export class UserService {
-  // Import du modèle User
+  // Import du modèle user
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  // Méthode create pour créer un utilisateur en BDD
-  async create(createUserDto: CreateUserDto): Promise<User> {
-    const createdCat = new this.userModel(createUserDto);
-    return createdCat.save();
+  // Méthode pour créer un utilisateur
+  async createUser(req: CreateUserDto): Promise<User> {
+    // Recherche si le user existe
+    const existingUser = await this.userModel.findOne({ email: req.email });
+    if (existingUser) throw new ConflictException("User already exists.");
+
+    // Création du user
+    const createdUser = new this.userModel(req);
+    return createdUser.save();
   }
 }
