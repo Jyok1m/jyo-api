398051c8a11f86a6bd1b2a9febe72fae429a0606
Joachim Jasmin <joachim.jasmin@gmail.com>
2024-03-06 09:41:56 +0100

    fix: removed wrong imports and usage

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
-import { UserModule } from './user/user.module';
 import { UserModule } from './user/user.module';
 
 import * as mongoose from 'mongoose';
@@ -51,9 +48,9 @@ import * as mongoose from 'mongoose';
   ],
 
   // Controllers
-  controllers: [AppController, UserController],
+  controllers: [AppController],
 
   // Providers
-  providers: [AppService, UserService],
+  providers: [AppService],
 })
 export class AppModule {}
