1aff246c0ba5a97de733e8ac8aab02e87e5655f6
Joachim Jasmin <joachim.jasmin@gmail.com>
2024-03-06 09:26:45 +0100

    feat: configured DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
-import { MongooseModule, MongooseModuleFactoryOptions } from '@nestjs/mongoose';
-
-import mongoose from 'mongoose';
-
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './modules/user/user.controller';
-import { UserService } from './modules/user/user.service';
+
+import * as mongoose from 'mongoose';
 
 @Module({
   imports: [
     // Dotenv configuration
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-    }),
+    ConfigModule.forRoot(),
 
     // MongoDB connection config
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (
-        configService: ConfigService,
-      ): Promise<MongooseModuleFactoryOptions> => {
+      useFactory: async (configService: ConfigService) => {
         const uri = configService.get<string>('DB_CONNECTION_STRING');
 
         if (!uri) {
@@ -31,24 +23,31 @@ import { UserService } from './modules/user/user.service';
           );
         }
 
-        try {
-          await mongoose.connect(uri, { connectTimeoutMS: 2000 });
-          console.log('Successfully connected to the DB');
-          return { uri }; // return the MongooseModuleFactoryOptions object
-        } catch (e) {
-          console.error('Error connecting to the DB:', e.message);
-          // If an error occurs, let it propagate rather than returning an invalid state
-          throw e;
-        }
+        // Logging the success message here
+        console.log('Successfully connected to the Jyogames API DB 🥳');
+
+        // Return the MongooseModuleFactoryOptions object with the URI
+        return {
+          uri,
+          connectionFactory: async () => {
+            try {
+              // Connection is managed by MongooseModule
+              return mongoose.createConnection(uri, { connectTimeoutMS: 2000 });
+            } catch (e) {
+              console.error('Error connecting to the DB:', e.message);
+              throw e; // Propagate the error
+            }
+          },
+        };
       },
       inject: [ConfigService],
     }),
   ],
 
   // Controllers
-  controllers: [AppController, UserController],
+  controllers: [AppController],
 
   // Providers
-  providers: [AppService, UserService],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-const corsOptions = {
-  origin: function (origin: any, callback: any) {
-    const allowedOrigins = [process.env.FRONTEND_URL_LOCAL];
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  credentials: true,
-};
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+  const allowedOrigins = [configService.get('FRONTEND_URL_LOCAL')];
+
+  const corsOptions = {
+    origin: function (
+      origin: string | undefined,
+      callback: (error: Error | null, allow?: boolean) => void,
+    ) {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  };
+
   app.enableCors(corsOptions);
 
-  await app.listen(4000);
+  const port = parseInt(configService.get<string>('PORT', '4000'));
+  await app.listen(port);
 }
+
 bootstrap();
