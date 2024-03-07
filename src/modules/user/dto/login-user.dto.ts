bdc4c83d26ace5c0e42c49e54f4620a8f29e820c
Joachim Jasmin <joachim.jasmin@gmail.com>
2024-03-07 11:28:57 +0100

    modified user login DTO

--- src/modules/user/dto/login-user.dto.ts
+++ src/modules/user/dto/login-user.dto.ts
@@ -1,12 +1,8 @@
-import { IsEmail, IsString, Length } from "class-validator";
+import { IsString } from "class-validator";
 
 export class LoginUserDto {
   @IsString()
-  @Length(1, 50) // This sets a minimum length of 1 and a maximum of 50 characters
-  username?: string; // Optional - can either be username or email
-
-  @IsEmail()
-  email?: string; // Optional - can either be username or email
+  identifier: string; // Optional - can either be username or email
 
   @IsString()
   password: string; // No need for regex validation here
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,15 @@
-import { Injectable, ConflictException, InternalServerErrorException } from "@nestjs/common";
+import {
+  Injectable,
+  ConflictException,
+  InternalServerErrorException,
+  NotFoundException,
+  BadRequestException,
+} from "@nestjs/common";
 import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { User } from "src/schemas/user.schema";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { LoginUserDto } from "./dto/login-user.dto";
 import { v4 as uuidv4 } from "uuid";
 import * as bcrypt from "bcrypt";
 
@@ -32,6 +39,32 @@ export class UserService {
       return createdUser.save();
     } catch (e) {
       if (e instanceof ConflictException) {
+        throw e;
+      } else {
+        throw new InternalServerErrorException(e.message);
+      }
+    }
+  }
+
+  async loginUser(req: LoginUserDto): Promise<any> {
+    try {
+      // Recherche si le user existe
+      const user = await this.userModel.findOne({ $or: [{ email: req.identifier }, { username: req.identifier }] });
+      if (!user) throw new NotFoundException("User not found");
+
+      // Vérfication du mot de passe
+      const isMatch = await bcrypt.compare(req.password, user.password);
+      if (!isMatch) throw new BadRequestException("Incorrect password");
+
+      // Génération du JSON web token
+      const token = "blabla";
+
+      // Sauvegarde du token en DB
+  
+
+      return { token };
+    } catch (e) {
+      if (e instanceof NotFoundException) {
         throw e;
       } else {
         throw new InternalServerErrorException(e.message);
